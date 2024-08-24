ccfccce2554a8649fc146f04a8485632d57b53f6
Gan Ainm <gan.ainm.riomhphost@gmail.com>
2024-08-24 11:14:05 +0200

    Switch to use-package

--- init.el
+++ init.el
@@ -90,18 +90,20 @@
 ;;;
 ;;; theming
 ;;;
-(require 'doom-themes)
+(use-package doom-themes
+  :init
+  (load-theme 'doom-one :no-confirm)
 ;; (load-theme 'doom-solarized-dark t)
 ;; (require 'modus-themes)
 ;; (load-theme 'modus-vivendi-tinted)
 ;;(load-theme 'ef-night)
-(load-theme 'doom-one :no-confirm)
-(unless crafted-startup-inhibit-splash
-  (setq initial-buffer-choice #'crafted-startup-screen))
+  (unless crafted-startup-inhibit-splash
+    (setq initial-buffer-choice #'crafted-startup-screen)))
 
 ;; activate which-key
-(require 'which-key)
-(which-key-mode 1)
+(use-package which-key
+  :init
+  (which-key-mode 1))
 
 (require 'ement)
 (require 'vterm)
